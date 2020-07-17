d4f023d0a42c6c3805263ecfadf6528264b05ab8
Devon Wayman <devonwayman97@gmail.com>
2020-07-17 00:29:39 -0400

    Major updates
    Variety of major updates.

---
+++ Assets/Models/MainMenuScene/Laterns and candles/scripts/CameraFacing.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFacing : MonoBehaviour {
+	[SerializeField] private Camera cam;
+	
+	void Awake() {
+		cam = Camera.main; 
+	}
+	void FixedUpdate() {
+		Vector3 v = cam.transform.position - transform.position;
+		v.x = v.z = 0.0f;
+		transform.LookAt(cam.transform.position - v); 
+	}
+}
