c323c23b45e92669ba3624c5fce4d3b8e09b789f
Devon Wayman <devonwayman97@gmail.com>
2020-12-13 01:58:20 -0500

    Removed Alembic support and refactored -DW
    Heavily refactored scene management system and removed alembic cache file support.

---
+++ Assets/Scripts/SceneManagement/SceneIndexes.cs
@@ -0,0 +1,10 @@
+﻿// Author Devon Wayman 
+// Date 12/13/2020
+
+// Scenes in build settings with their corresponding build index numbers
+namespace SceneManagement {
+    public enum SceneIndexes {
+        MAIN_MENU = 1,
+        STORY_SCENE = 2,
+    }
+}
---
+++ Assets/Scripts/Visuals/SelectBookTexture.cs
@@ -0,0 +1,25 @@
+﻿// Author: Devon Wayman
+using UnityEngine;
+
+/// <summary>
+/// Selects random diffuse texture for the opening menu book
+/// </summary>
+namespace Visuals {
+    public class SelectBookTexture : MonoBehaviour {
+        
+        // Renderer attatched to book object on main menu
+        private Renderer bookRenderer = null; 
+
+        // Array of starting book textures
+        private Texture[] bookTextures = null; 
+        
+        private void Awake() {
+            bookTextures = Resources.LoadAll<Texture2D>("Textures/StartBookEntries");
+
+            bookRenderer = GetComponent<Renderer>();
+
+            int selection = Random.Range (0, bookTextures.Length);
+            bookRenderer.material.mainTexture = bookTextures[selection];
+        }
+    }
+}
